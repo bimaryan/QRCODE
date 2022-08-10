fcdecebd2be8145012698d2eea8c36161eb6cf63
bimaryan <74827383+bimaryan@users.noreply.github.com>
2022-08-10 11:54:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,10 @@
   <title>QR CODE</title>
   <style>
     #result{
-      height: 300px;
-      width: 300px;  
+      height: 315px;
+      width: 315px; 
+      background-color: black;
+      
     }
 </style>
   <script nomodulewindow.MSInputMethodContext && document.documentMode && document.write('<link rel="stylesheet"
@@ -15,12 +17,12 @@
   <script src="https://cdn.jsdelivr.net/npm/ie11-custom-properties@4/ie11CustomProperties.min.js" </\script> </script>
 </head>
   
-<body class="bg-dark">
+<body class="bg-info">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   <!--Navbar-->
-  <nav class="navbar navbar-expand-lg bg-light">
+  <nav class="navbar navbar-expand-lg fixed-top bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Generate Your QR</a>
+    <a class="navbar-brand" href="#">Generate QR</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -35,21 +37,24 @@
                   </a>
                   <ul class="dropdown-menu dropdown-menu-light">
                     <li><a class="dropdown-item" href="https://www.instagram.com/bima_ryan23/">Author</a></li>
-                    <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#exampleModal">Github</a>
+                    <li><a class="dropdown-item" href="https://github.com/bimaryan/QRCODE" target="_blank">Github</a>
                     </li>
                   </ul>
                 </li>
               </ul>
-            </div>
+        </div>
       </div>
     </div>
   </div>
 </nav>
+  <br>
+  <br>
+  <br>
   <!--content-->
   <div class="container">
-        <center class="mt-2">
-          <div id="result">
-            <iframe name="qrcode-frame" frameborder="1"  id="qrcode" src="gen.php" height="350px;" width="350px"></iframe>
+        <center>
+          <div id="result" class="shadow">
+            <iframe name="qrcode-frame" frameborder="1"  id="qrcode" src="gen.php" height="320px;" width="320px"></iframe>
           </div>
         </center>
         
@@ -70,28 +75,29 @@
                 <input type="radio" name="size" value="300x300"><span>300x300</span>
               </div>
           </div>
-          <!--<div>
+          <div>
             <legend>Encoding:</legend>
             <input type="radio" name="encoding" value="UTF-8" checked>UTF-8<br>
             <input type="radio" name="encoding" value="Shift_JIS">Shift_JIS<br>
             <input type="radio" name="encoding" value="ISO-8859-1">ISO-8859-1<br>
-          </div>-->
-          <div>
-            <p><strong>Link :</strong></p>
-            <input type="text" class="form-control me-2" id="exampleInputEmail1" name="content">
+          </div>
+          <div class="mt-2">
+            <p><strong>Submit URL :</strong></p>
+            <input type="text" class="form-control me-2" name="content" placeholder="https://">
           </div>   
-            <br>
-          <!--<div>
-            <select name="correction">
+          <div class="btn-group">
+            <select name="correction" class="form-select btn-btn-light mt-3">
                 <option value="L" selected>L</option>
                 <option value="M">M</option>
                 <option value="Q">Q</option>
                 <option value="H">H</option>
             </select>
-          </div>-->
-            <input type="submit" value="Generate QR Code" class="btn btn-success"></input>
+          </div>
+            <br>
+            <input type="submit" value="Generate" class="btn btn-success mt-3">
         </form>
     </div> 
 </div>
+  <script src="index.js"></script>
 </body>
 </html>
